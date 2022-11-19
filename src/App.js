26bb41ee781567b73a82d47111aa286d7c1f20a1
Yamanadm <adsamu44@gmail.com>
2022-11-19 11:39:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Contacts from "./components/contacts/Contacts";
+import FormComponent from "./components/form/FormComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
